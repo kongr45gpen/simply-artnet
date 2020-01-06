762009d0eb2499f36ca391e452c865dfe1992b03
kongr45gpen <electrovesta@gmail.com>
2020-01-07 00:06:52 +0200

    Update for python 3

--- artnet.py
+++ artnet.py
@@ -23,52 +23,52 @@ def chanval(chan, val):
 	sys.stdout.write("\r%d -> %d  \t [master@%f]" % (chan,val,0))
 	sys.stdout.flush()
 
-print "Hello"
+print("Hello")
 
 dmxstates = []
 dmxinit = False
 
-for i in xrange(1,514):
+for i in range(1,514):
 	dmxstates.append(-1)
 
 def lhex(h):
-    return ':'.join(x.encode('hex') for x in h)
+    return ':'.join("%x" % x for x in h)
 def setDmxValue(i, val):
 	if dmxstates[i] == -1:
 		dmxstates[i] = val
 	if dmxstates[i] != val:
 		dmxstates[i] = val
 		# DMX UPDATE!!! WOW!!!
-		print "Setting channel %d to %d" % (i,ord(val))
-		chanval(i,ord(val))
-	
+		print("Setting channel %d to %d" % (i,val))
+		chanval(i,val)
+
 while True:
   data = sock.recv(10240)
-  
-  if len(data) < 20:
-	continue
 
-  if data[0:7] != "Art-Net" or data[7] != "\0":
-	# artnet package
-	continue
-  
-  if ord(data[8]) != 0x00 or ord(data[9]) != 0x50:
-	# OpDmx
-	continue
-  
-  protverhi = ord(data[10])
-  protverlo = ord(data[11])
-  sequence  = ord(data[12])
-  physical  = ord(data[13])
-  subuni    = ord(data[14])
-  net       = ord(data[15])
-  lengthhi  = ord(data[16])
-  length    = ord(data[17])
+  if len(data) < 20:
+    continue
+
+  if data[0:7] != b"Art-Net" or data[7] != 0:
+    # artnet package
+    continue
+
+  if data[8] != 0x00 or data[9] != 0x50:
+    # OpDmx
+    continue
+
+  protverhi = data[10]
+  protverlo = data[11]
+  sequence  = data[12]
+  physical  = data[13]
+  subuni    = data[14]
+  net       = data[15]
+  lengthhi  = data[16]
+  length    = data[17]
   dmx       = data[18:]
-  
-  for i in xrange(0,510):
-	setDmxValue(i+1,dmx[i])
-	
-  #print data[0:4]
-  
-  #print lhex(dmx)
+
+  for i in range(0,510):
+      setDmxValue(i+1,dmx[i])
+
+  #print(data[0:4])
+
+  #print(lhex(dmx))
